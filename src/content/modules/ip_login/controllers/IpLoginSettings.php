aba637f3208d472dec737320e2c2d554da467b49
Ulrich Schmidt <admin@deruli.de>
2018-01-16 07:36:35 +0100

    refakturiert

--- src/content/modules/ip_login/controllers/IpLoginSettings.php
+++ src/content/modules/ip_login/controllers/IpLoginSettings.php
@@ -1,10 +1,15 @@
 <?php
-class IpLoginSettings extends Controller {
-	const MODULE_NAME = "ip_login";
-	public function savePost() {
-		if (Request::getVar ( "ip_user_login", "", "str" )) {
-			Settings::set ( "ip_user_login", Request::getVar ( "ip_user_login", "", "str" ) );
-		}
-		Request::redirect ( ModuleHelper::buildAdminURL ( self::MODULE_NAME, "save=1" ) );
-	}
+
+class IpLoginSettings extends Controller
+{
+
+    const MODULE_NAME = "ip_login";
+
+    public function savePost()
+    {
+        if (Request::getVar("ip_user_login", "", "str")) {
+            Settings::set("ip_user_login", Request::getVar("ip_user_login", "", "str"));
+        }
+        Request::redirect(ModuleHelper::buildAdminURL(self::MODULE_NAME, "save=1"));
+    }
 }
--- src/post-install.php
+++ src/post-install.php
@@ -1,2 +1,2 @@
 <?php
-Settings::register ( "ip_user_login", "" );
+Settings::register("ip_user_login", "");
